0efd31d8d4bc51bbcf94834be2286e81ad62fdcc
yassuhacks750 <yassudj8085231272@gmail.com>
2025-10-05 16:20:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,16 +4,16 @@ from os import getenv
 
 
 # ------------------------------------------------
-API_ID = int(os.environ.get("API_ID", "27567486"))
+API_ID = int(os.environ.get("API_ID", "22849789"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH","b1760d4b5ef697bb8da4e7ac4e261c49")
+API_HASH = os.environ.get("API_HASH","0fc127c6055acd59f00ec6c229e1e3c4")
 # ------------------------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8054981115:AAEexuU-cWgy-6wwzLuz8xTGP19tVkfrYEg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8352615146:AAHi0skmzj_NjDB05JFs_jfSTbZyeIkNSOE")
 # ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "@aaaaanimaltxtbot")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@txtnewyashbot")
 BOT_TEXT = "newtxtbot"
 # ------------------------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "7780806801"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7296271316"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002770753690"))
@@ -21,32 +21,16 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002770753690"))
 # //FORCE_CHANNEL_ID
 CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002471859014")) 
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://editingtution99:kLKimOFEX1MN1v0G@cluster0.fxbujjd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://yashxug:Yash2835P@yashxug.om3yk1q.mongodb.net/retryWrites=true&w=majority&appName=Yashxug")
 # -----------------------------------------------
 PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002770077524"))
 # -----------------------------------------------
-join = '<a href="https://t.me/Alphasto_09_bot">✳️ JOIN BACKUP</a>'
+join = '<a href="https://t.me/Alphaso_09_bot">✳️ JOIN BACKUP</a>'
 # -----------------------------------------------
 UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
 # -----------------------------------------------
 UNSPLASH_QUERY = 'animal baby'
 # -----------------------------------------------
-ADMIN_BOT_USERNAME = "newtxtbot" #without @
+ADMIN_BOT_USERNAME = "txtnewyashbot" #without @
 
 THUMB_URL = os.environ.get("THUMB_URL", "https://img.freepik.com/premium-photo/black-car-is-driving-down-road_951586-147043.jpg")
-
-
-
-
-# # Bot configuration
-# API_ID = int(os.environ.get("API_ID", "22746239"))
-# API_HASH = os.environ.get("API_HASH", "a98ec8cfd8572a3a7c936cf828fe6215")
-# BOT_TOKEN = os.environ.get("BOT_TOKEN", "7547829346:AAGyfvOu47EciNhC7NUGSDEDFuBaetYYusw")
-# BOT_USERNAME = os.environ.get("BOT_USERNAME", "MassRPBot")
-# OWNER_ID = int(os.environ.get("OWNER_ID", "7463601722"))
-# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7463601722").split()))
-# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002601604234"))
-# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002601604234"))
-# THUMB_URL = os.environ.get("THUMB_URL", "https://i.fbcd.co/products/original/ug-circle-logo-design-2-e84695ca2ab9a697d2b2d7c928b0bf5f12bf18e076da241815e0372c8d617915.jpg")
-
